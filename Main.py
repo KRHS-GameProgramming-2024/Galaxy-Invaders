51e106db9aa7a9d02e72eb52bdb66bb4d9c31e9f
andrew cushing <andrew.cushing@kearsarge.org>
2024-02-14 08:27:17 -0500

    code

--- Main.py
+++ Main.py
@@ -1,5 +1,62 @@
-import pygame, sys, math
+import pygame, sys, math, random
 from Enemy import *
 from Player import *
+from Player2 import *
+
+pygame.init()
+clock = pygame.time.Clock();
+
+size= [1500, 800]
+screen = pygame.display.set_mode(size)
 
 
+
+counter = 0;
+player = PlayerBall(4, [1450/2, 1250/2])
+balls = [player]
+
+while True:
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            sys.exit();
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a or event.key == pygame.K_LEFT:
+                player.goKey("left")
+            elif event.key == pygame.K_d or event.key == pygame.K_RIGHT:
+                player.goKey("right")
+
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a or event.key == pygame.K_LEFT:
+                player.goKey("sleft")
+            elif event.key == pygame.K_d or event.key == pygame.K_RIGHT:
+                player.goKey("sright")
+
+            
+    counter += 1
+    if counter >= 100:
+        counter = 0;
+        balls += [Ball("Enemyship.png", 
+                [random.randint(-7,7), random.randint(-7,7)],
+                [random.randint(100,700), random.randint(100, 500)])
+          ]
+        for ball in balls: 
+            if balls[-1].ballCollide(ball):
+                balls.remove(balls[-1])
+                break
+    
+    
+    for ball in balls:
+        ball.move()
+        ball.wallCollide(size)
+        
+    for hittingBall in balls:
+        for hitBall in balls:
+            hittingBall.ballCollide(hitBall)
+    
+
+    screen.fill((97, 164, 229))
+    for ball in balls:
+        screen.blit(ball.image, ball.rect)
+    pygame.display.flip()
+    clock.tick(60)
+    print(clock.get_fps()) 
--- Player.py
+++ Player.py
@@ -1,2 +1,18 @@
 import pygame, sys, math
+from Player2 import *
 
+class PlayerBall(Ball):
+    def __init__(self,maxSpeed=4, startPos=[0,0]):
+        Ball.__init__(self, "player.png", [0,0], startPos)
+        self.maxSpeed = maxSpeed
+
+    def goKey(self, direction):
+        if direction == "left":
+            self.speedx = -self.maxSpeed
+        elif direction == "right":
+            self.speedx = self.maxSpeed
+        elif direction == "sleft":
+            self.speedx = 0
+        elif direction == "sright":
+            self.speedx = 0
+            
