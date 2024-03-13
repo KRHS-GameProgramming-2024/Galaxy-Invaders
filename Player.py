88d5a9e419f348d922b7f5a595243fc8d29bb323
Austin Needham <austin.needham@kearsarge.org>
2024-03-13 08:03:53 -0400

    clean up and bullets

--- Player.py
+++ Player.py
@@ -1,5 +1,6 @@
 import pygame, sys, math
-from Player2 import *
+from Ship import *
+from Bullet import *
 
 class PlayerShip(Ship):
     def __init__(self,maxSpeed=15, startPos=[0,0]):
@@ -16,21 +17,7 @@ class PlayerShip(Ship):
         elif direction == "sright":
             self.speedx = 0
             
-  
-    class Player:
-        def __init__(self):
-            self.position = [0,0]
-            self.direction = [1,
-    0]
-            self.shoot_cooldown = 0.5
-            self.last_shot_time = 0
-            
     def shoot(self):
-        current.time() == time.time()
-    if current_time - self.last_shot_time > self.shoot_cooldown:
-        import time
-        player = Player()
-    while True:
-        user_input = input("Press 'W' to shoot")
-    if user_input.lower() == "w":
-        player.shoot()
+        return Bullet([0, -10], self.rect.center)
+            
+  
