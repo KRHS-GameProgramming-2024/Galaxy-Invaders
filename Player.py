24fcfd5555bdcf161522f19dba0c1d2182b96115
Austin Needham <austin.needham@kearsarge.org>
2024-03-13 07:47:51 -0400

    made player faster

--- Player.py
+++ Player.py
@@ -2,7 +2,7 @@ import pygame, sys, math
 from Player2 import *
 
 class PlayerShip(Ship):
-    def __init__(self,maxSpeed=4, startPos=[0,0]):
+    def __init__(self,maxSpeed=15, startPos=[0,0]):
         Ship.__init__(self, "player.png", [0,0], startPos)
         self.maxSpeed = maxSpeed
 
