b3b06b65683b96fdaa880ebb4cee32b275bb58da
andrew cushing <andrew.cushing@kearsarge.org>
2024-04-12 07:58:54 -0400

    Start of enemy bullet code

--- Bullet.py
+++ Bullet.py
@@ -5,3 +5,8 @@ class Bullet(Ship):
     def __init__(self,speed, startPos=[0,0]):
         Ship.__init__(self, "Bullet/Bullet.png", speed, startPos)
         self.rad = (self.rect.height/50 + self.rect.width/50)/50
+        
+class Bullet(Ship):
+    def __init__(self,speed, startPos=[0,0]):
+        Ship.__init__(self, "Bullet/Bullet.png", speed, startPos)
+        self.rad = (self.rect.height/50 + self.rect.width/50)/50
