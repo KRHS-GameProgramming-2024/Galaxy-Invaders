c32e7e92d318c14e28a9669c4dcfe0257fe3518c
Austin Needham <austin.needham@kearsarge.org>
2024-04-12 07:58:39 -0400

    fixing the bullet and the players speed also the bullet size.

--- Bullet.py
+++ Bullet.py
@@ -4,4 +4,4 @@ from Ship import *
 class Bullet(Ship):
     def __init__(self,speed, startPos=[0,0]):
         Ship.__init__(self, "Bullet/Bullet.png", speed, startPos)
-        self.rad = (self.rect.height/20 + self.rect.width/20)/20
+        self.rad = (self.rect.height/50 + self.rect.width/50)/50
