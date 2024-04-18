e532e2b2e224bd7fd7347c67cddfd54be5961333
Bryce <bryce.tibbetts@kearsarge.org>
2024-04-18 10:56:26 -0400

    fixed collishons

--- Bullet.py
+++ Bullet.py
@@ -5,8 +5,14 @@ class Bullet(Ship):
     def __init__(self,speed, startPos=[0,0]):
         Ship.__init__(self, "Bullet/Bullet.png", speed, startPos)
         self.rad = (self.rect.height/50 + self.rect.width/50)/50
-        
-class Bullet(Ship):
-    def __init__(self,speed, startPos=[0,0]):
-        Ship.__init__(self, "Bullet/Bullet.png", speed, startPos)
-        self.rad = (self.rect.height/50 + self.rect.width/50)/50
+    
+    
+    
+    def shipCollide(self,ship):
+        if ship.rect.left <= self.rect.right:
+            if ship.rect.right >= self.rect.left:
+                if ship.rect.top <= self.rect.bottom:
+                    if ship.rect.bottom >= self.rect.top:
+                        print('collishon')
+                        return True
+        return False
