c3e679c7cc0f791b5a1cfb5bee0ccd8b92676588
Austin Needham <austin.needham@kearsarge.org>
2024-05-29 08:41:25 -0400

    made the hud making the kills change per kill.

---
+++ Hud.py
@@ -0,0 +1,14 @@
+import pygame, sys, math
+
+class HUD():
+    def __init__(self,basetext, startvalue, startPos=[0,0]):
+        self.font = pygame.font.Font(None, 64)
+        self.value=startvalue
+        self.basetext=basetext
+        self.image = self.font.render(self.basetext+" "+str(self.value), True, (255, 255, 255))
+        self.rect = self.image.get_rect(topleft=startPos)
+
+    def increase(self):
+        self.value+=1
+        self.image = self.font.render(self.basetext+" "+str(self.value), True, (255, 255, 255))
+        self.rect = self.image.get_rect()
